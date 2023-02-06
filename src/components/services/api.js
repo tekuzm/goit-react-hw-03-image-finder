3bc8a75e2e714303e576bbee4eb7ebb7f5156bd8
TetianaKuzma <tetiana.b97@gmail.com>
2023-02-06 15:35:48 +0200

    Add readme

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -2,5 +2,21 @@ import axios from 'axios';
 
 const API_KEY = '32114919-b1da84808ca0f604bcef4a7c4';
 
-axios.defaults.baseURL =
-  'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const fetchImages = async q => {
+  const response = await axios(
+    `?q=${q}&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12`,
+    {
+      params: {
+        q,
+        key: API_KEY,
+      },
+    }
+  );
+
+  console.log(response);
+  return;
+};
+
+export default fetchImages;
